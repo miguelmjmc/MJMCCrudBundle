88dda760ef9652e226ff223b146432f4202a2ee0
miguelmjmc <miguel.mjmc@gmail.com>
2019-09-26 14:29:39 -0400

    Add: Bundle skeleton

---
+++ MJMCCrudBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MJMC\Bundle\CrudBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MJMCCrudBundle extends Bundle
+{
+}
---
+++ Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MJMC\Bundle\CrudBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+}
